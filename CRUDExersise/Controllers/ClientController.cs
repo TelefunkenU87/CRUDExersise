0c301b7f2eb72efcf7b692d629bd99cbc4aadc7d
telefunk <telewebb@gmail.com>
2020-05-18 15:18:44 -0500

    Implemented edit and delete controler method and views for Clients

--- CRUDExersise/Controllers/ClientController.cs
+++ CRUDExersise/Controllers/ClientController.cs
@@ -10,6 +10,7 @@ namespace CRUDExersise.Controllers
     public class ClientController : Controller
     {
         private readonly IClientRepository _clientRepository;
+        
         public ClientController(IClientRepository clientRepository)
         {
             _clientRepository = clientRepository;
@@ -37,6 +38,65 @@ namespace CRUDExersise.Controllers
                 ViewBag.Message = TempData["Message"].ToString();
             }
             return View(client);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var client = new Client();
+            if(id.HasValue)
+            {
+                client = _clientRepository.GetClientById(id.Value);
+            }
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Client updatedClient)
+        {
+            int Id = 0;
+            if (updatedClient.ClientId > 0)
+            {
+                var client = _clientRepository.Update(updatedClient);
+                _clientRepository.Commit();
+                Id = client.ClientId;
+            }
+            else
+            {
+                var client = _clientRepository.Add(updatedClient);
+                _clientRepository.Commit();
+                Id = client.ClientId;
+            }
+            TempData["Message"] = "Client data saved";
+            return RedirectToAction("Details", new { id = Id });
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var client = _clientRepository.GetClientById(Id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int Id)
+        {
+            var client = _clientRepository.Delete(Id);
+            _clientRepository.Commit();
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+            TempData["Message"] = $"{client.ClientName} was successfully deleted";
+            return RedirectToAction("List");
         }
     }
 }
